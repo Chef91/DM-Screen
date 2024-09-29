6f7559e903381f06852b6258866494e46d390629
Chef91 <chefgraham1991@gmail.com>
2024-09-29 00:01:52 -0500

    9.29.2024
    update 1

--- python_projects/project.py
+++ python_projects/project.py
@@ -3,8 +3,10 @@ from tkinter import *
 root = Tk()
 
 # crating a label widget
-myLabel = Label(root, text="Hello Adventurer!")
-# shoving it onto the screen
-myLabel.pack()
+myLabel1 = Label(root, text="Hello Adventurer!")
+myLabel2 = Label(root, text="Roll Dice")
+
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
 
 root.mainloop()
